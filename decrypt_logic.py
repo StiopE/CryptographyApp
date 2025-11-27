b4f6f1973b436c377d2c756f5055244be51b97e9
BrTrGr <bruno.triano@gmail.com>
2025-11-27 17:43:24 +0100

    recode

---
+++ decrypt_logic.py
@@ -0,0 +1,72 @@
+import customtkinter as ctk
+from cryptography.fernet import Fernet
+
+# Function to use the key and ciphertext
+def decrypt(entry_password_widget, entry_decrypt_widget):
+    # Get the values from the widgets passed to this function
+    key = entry_password_widget.get()
+    ciphertext = entry_decrypt_widget.get()
+    
+    # Use the values ciphertext and key for decryption
+    f = Fernet(key.encode())
+    decrypted_bytes = f.decrypt(ciphertext.encode())
+    decrypted_plaintext = decrypted_bytes.decode()
+    
+    # Create the results window
+    root4 = ctk.CTk()
+    root4.title = "Decrypted message"
+    root4.geometry('200x200')
+    
+    # Configure grid
+    indices = [0, 1, 2, 3, 4] 
+    for i in indices:
+        root4.grid_columnconfigure(i, weight=1)
+        root4.grid_rowconfigure(i, weight=1)
+    
+    final_decrypted_message = ctk.CTkLabel(root4, text=f'Decrypted message: {decrypted_plaintext}')
+    final_decrypted_message.grid(row=0, column=1)
+    
+    root4.mainloop()
+
+# Open decryptic window
+def decrypt_window():
+    # System settings 
+    ctk.set_appearance_mode("System")
+    ctk.set_default_color_theme("blue")
+    
+    # First window configuration
+    root2 = ctk.CTk()
+    root2.title("Decryption Menu")
+    root2.geometry('600x400')
+    
+    # Adding padding system LIST
+    indices = [0, 1, 2, 3, 4] 
+    for i in indices:
+        root2.grid_columnconfigure(i, weight=1)
+        root2.grid_rowconfigure(i, weight=1)
+    
+    # Create buttons using TUPLE
+    header_font_style = ("Helvetica", 20) 
+    decrypt_label2 = ctk.CTkLabel(root2, text= "Encryption", font=header_font_style)
+    decrypt_label2.grid(row = 0, column = 2)
+   
+    label_message = ctk.CTkLabel(root2, text="Enter text to decrypt:")
+    label_message.grid(row=1, column=1)
+    
+    entry_decrypt = ctk.CTkEntry(root2, placeholder_text="Enter message")
+    entry_decrypt.grid(row = 1, column=3)
+    
+    label_password = ctk.CTkLabel(root2, text="Enter key:")
+    label_password.grid(row=2, column=1)
+    
+    entry_password = ctk.CTkEntry(root2, placeholder_text="Enter key")
+    entry_password.grid(row=2, column=3)
+    
+    submit_button = ctk.CTkButton(
+        root2, 
+        text="Submit",
+        command=lambda: decrypt(entry_password, entry_decrypt)
+    )
+    submit_button.grid(row=4, column=2)
+
+    root2.mainloop()
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import methods
+import main_window
 
 # Main function
 def main():
     # Call the main menu function
-    methods.init_app()
+    main_window.init_app()
 
 if __name__ == "__main__":
     main()
